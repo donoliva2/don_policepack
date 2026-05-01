9a172402418c949d50e8b6967d29d5a0bfdece52
DON <167806587+donoliva2@users.noreply.github.com>
2026-05-02 01:02:52 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.Rewards = {
+    { item = 'weapon_pistol', amount = 1 },
+    { item = 'ammo-9', amount = 50 },
+    { item = 'handcuffs', amount = 1 },
+    { item = 'radio', amount = 1 },
+}
+
+Config.RequiredJob = "police"
